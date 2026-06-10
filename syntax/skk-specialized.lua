b9d9ac2deb89bc8ae52894b03d49decac174804c
STG <stg737467@gmail.com>
2026-06-10 14:59:53 +0900

    fix: 余計なメッセージを表示しない

--- syntax/skk-specialized.lua
+++ syntax/skk-specialized.lua
@@ -6,8 +6,6 @@ vim.b.skk_bunnrui = vim.b.skk_bunnrui or require("skk").get.bunnrui_from_table(v
 
 if vim.b.skk_bunnrui then
     vim.cmd.syntax([[match bunnruiNotFound_kouho_skk '\v\/@<=([^/]+;\[]] .. vim.b.skk_bunnrui .. [[\])@![^/]+' contained containedin=kouho_skk]])
-else
-    vim.notify('候補の分類を取得できませんでした 以下を実行してください :let b:skk_bunnrui = hoge | set ft=skk-specialized',vim.log.levels.WARN)
 end
 
 vim.api.nvim_set_hl(0,'bunnruiNotFound_kouho_skk',{link = 'error'})
